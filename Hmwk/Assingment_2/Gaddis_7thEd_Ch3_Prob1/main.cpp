29955f6b7f21ab047abbe985944a2cfb76e2354f
narudkram49 <narudkram49@gmail.com>
2014-06-30 13:44:59 -0700

    Signed-off-by: narudkram49 <narudkram49@gmail.com>

---
+++ Hmwk/Assingment_2/Gaddis_7thEd_Ch3_Prob1/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Mark Duran
+ * Purpose: Miles per Gallon
+ * Created on June 30, 2014, 12:14 PM
+ */
+
+//system libraries
+#include <iostream>
+using namespace std;
+
+//use libraries
+
+//global constants
+
+//function prototypes
+
+//execution begins here
+int main(int argc, char** argv) {
+    //declare the variables
+      int gallons;
+      int miles;
+      unsigned short mpg;
+      
+      
+    return 0;
+}
+
