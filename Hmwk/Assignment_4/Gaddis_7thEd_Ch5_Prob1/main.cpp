aebdde1646281fa4ea74f5335d941e6b635df29d
narudkram49 <narudkram49@gmail.com>
2014-07-10 13:46:00 -0700

    Signed-off-by: narudkram49 <narudkram49@gmail.com>

---
+++ Hmwk/Assignment_4/Gaddis_7thEd_Ch5_Prob1/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Mark Duran
+ * Created on July 10, 2014, 1:06 PM
+ * Purpose: Sum of Numbers
+ */
+
+//system level libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//global 
+
+//function prototypes
+
+//execution starts here
+int main(int argc, char** argv) {
+    //declare variables
+    unsigned int var1;
+    unsigned int num;
+    
+    cout<<"Enter a positive integer value\n";
+    cin>>var1;
+    
+    
+    
+    return 0;
+}
+
---
+++ Projects/Project_1/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Mark Duran
+ * Created on July 10, 2014, 10:44 AM
+ * Purpose:
+ */
+
+//system level libraries
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+//user libraries
+
+//global 
+
+//function prototypes
+
+//execution starts here
+int main(int argc, char** argv) {
+    
+    //declare variables
+    char choice1;
+    char Y;
+    char N;
+    
+    //start story
+    cout<<"You find yourself in a dark room\n";
+    Sleep(1500);
+    cout<<"You make out the outline of a door in front of you\n";
+    Sleep(1500);
+    cout<<"Do you go through it? Y/N\n";
+    Sleep(1500);
+    cin>>choice1;
+    if (choice1==Y)
+        cout<<"You go through the door a bright light assaults your eyes\n"   
+    else if        
+    return 0;
+}
+
